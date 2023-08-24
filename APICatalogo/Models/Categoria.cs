2f45d539e5b90551fd39f575d7c99d803dc70123
Caio Martinelli <caio.martinelli@gmail.com>
2023-08-24 18:57:36 -0300

    Utilizando Data Annotations e executando o migration para ajustar as tabelas.

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [MaxLength(300)]
     public string? ImagemUrl { get; set; }
     //Criando a coleção de produtos, irá fazer o vínculo de produtos e categorias
     public ICollection<Produto>? Produtos { get; set; }
